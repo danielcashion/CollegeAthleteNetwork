995501c32ba08c20c33aaa9dcdf75bb9c41c34e4
The Club Sports Organization <admin@clublacrosse.org>
2025-01-19 23:59:12 -0500

    fixes to Yale Mens Lax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import "../styles/customStyles.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import Carousel from "@/components/LandingPage/Carousel";
-// import AthleteImage from "../../public/images/college-athletes-1.jpg";
-import AthleteImage from "../../public/images/Yale.jpg";
+import AthleteImage from "../../public/images/YaleMensLax.jpeg";
 
 import {
   FaDatabase,
