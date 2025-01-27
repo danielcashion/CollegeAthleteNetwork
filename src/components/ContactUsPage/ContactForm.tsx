8acf4b678e5429990f19baad29f96e2500b3d1e3
danielcashion <daniel.cashion.nyc@gmail.com>
2025-01-27 14:37:18 -0500

    zoom on map

--- src/components/ContactUsPage/ContactForm.tsx
+++ src/components/ContactUsPage/ContactForm.tsx
@@ -102,7 +102,7 @@ const ContactForm = () => {
           </Link>
         </div>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3851.19875304904!2d-74.0127638!3d40.7124175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25b5b489d1a5f%3A0xd961b48e6ec74116!2sThe%20Club%20Sports%20Organization%20LLC!5e1!3m2!1sen!2sus!4v1738003747865!5m2!1sen!2sus"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3851.19875304904!2d-74.0127638!3d40.7124175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25b5b489d1a5f%3A0xd961b48e6ec74116!2sThe%20Club%20Sports%20Organization%20LLC!5e1!3m2!1sen!2sus!4v1738003747865!5m2!1sen!2sus&z=5"
           allowFullScreen
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
