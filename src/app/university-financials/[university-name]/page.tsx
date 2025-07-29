1d1695b9eae1fbea03b8ee352976f2ff0bd531c0
danielcashion <daniel.cashion.nyc@gmail.com>
2025-07-29 19:21:00 -0400

    style: update financial contributions model heading for university-specific display

--- src/app/university-financials/[university-name]/page.tsx
+++ src/app/university-financials/[university-name]/page.tsx
@@ -20,10 +20,18 @@ export default async function UniversityFinancials({
   return (
     <div>
       <div className="bg-gradient-to-r text-center from-[#1C315F] to-[#ED3237] text-white pb-12 pt-24 flex flex-col items-center px-[10%] sm:px-[20%]">
-        <h1 className="text-4xl md:text-5xl font-bold mb-4">
-          Financial Contributions Model for{" "}
-          <span className="capitalize">{universityName}</span>
-        </h1>
+        {teams[0]?.university_name === "Typical University" && (
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
+            Financial Contributions Model: {" "}
+            <span className="capitalize">{universityName} *</span>
+          </h1>
+        )}
+        {teams[0]?.university_name !== "Typical University" && (
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
+            Financial Contributions Model: {" "}
+            <span className="capitalize">{universityName}</span>
+          </h1>
+        )}
         <h2 className="text-3xl font-semibold mb-0 ml-20 mr-20">
           Does <span className="capitalize">{universityName}</span> receive any
           direct financial benefit
@@ -41,11 +49,13 @@ export default async function UniversityFinancials({
         </h2>
         {teams[0]?.university_name === "Typical University" && (
           <h2 className="text-md mb-2 mt-2">
-            * We have not mapped your university&apos;s data. Below are the averages for all universities in our datasets. Please{" "}
+            * We have not mapped your university&apos;s data. Below are the
+            averages for all universities in our datasets. Please{" "}
             <Link href="/contact-us" className="underline font-bold">
               Contact Us
             </Link>{" "}
-            if you want to us to load your university&apos;s Athlete Network data.
+            if you want to us to load your university&apos;s Athlete Network
+            data.
           </h2>
         )}
       </div>
