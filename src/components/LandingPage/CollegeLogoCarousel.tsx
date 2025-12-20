64b2cc2d3a21c481f6d32c9b3d1d267ebe8b59fc
danielcashion <daniel.cashion.nyc@gmail.com>
2025-12-20 11:08:54 -0500

    Remove Princeton

--- src/components/LandingPage/CollegeLogoCarousel.tsx
+++ src/components/LandingPage/CollegeLogoCarousel.tsx
@@ -11,9 +11,6 @@ const CollegeLogosCarousel = () => {
       src: "https://collegeathletenetwork.s3.us-east-1.amazonaws.com/Cornell.png",
     },
     {
-      src: "https://collegeathletenetwork.s3.us-east-1.amazonaws.com/Princeton.png",
-    },
-    {
       src: "https://collegeathletenetwork.s3.us-east-1.amazonaws.com/Logos/UNC.png",
     },
     {
