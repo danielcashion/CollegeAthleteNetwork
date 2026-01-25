c834e4347c6443c5dd51b7e2a4980f0cdd9415dd
danielcashion <daniel.cashion.nyc@gmail.com>
2026-01-25 11:54:56 -0500

    Refactor VideoViewerPage header and description for improved clarity and engagement; remove 'Get in Touch' link

--- src/app/video-viewer/[university_name]/page.tsx
+++ src/app/video-viewer/[university_name]/page.tsx
@@ -36,13 +36,12 @@ export default function VideoViewerPage() {
       <div className="bg-gray-50 flex flex-col items-center pb-4">
         <div className="w-full bg-gradient-to-r from-[#1C315F] to-[#ED3237] text-white pt-16 pb-5 text-center shadow-md">
           <h1 className="text-3xl mt-2 sm:text-4xl font-extrabold tracking-tight">
-            {`Video Library for The ${displayedUniversityName} Athlete Network`}
+            {`The ${displayedUniversityName} Athlete Network Video Library`}
           </h1>
 
           <p className="text-2xl mt-2 max-w-3xl mx-auto font-bold text-white/80">
-            Introducing the sophistication and engagement The College Athlete Network brings to
-            the {displayedUniversityName} Athletic Department & the{" "}
-            {displayedUniversityName} Athlete Network.
+            Delivering the simplicity, engagement, and convenience of The College Athlete Network to
+            the {displayedUniversityName} Athlete Network.
           </p>
         </div>
 
@@ -77,12 +76,6 @@ export default function VideoViewerPage() {
           Ready to engage with the {displayedUniversityName} Athlete Network?
         </div>
         <div className="mt-3 mb-2 flex flex-row flex-wrap items-center justify-center gap-4">
-          <Link
-            href="/contact-us"
-            className="bg-[#1C315F] text-[#FFFFFF] font-semibold px-6 py-2 rounded-full shadow hover:bg-[#ED3237] transition"
-          >
-            Get in Touch
-          </Link>
             <a
             href="https://members.collegeathletenetwork.org/login"
             className="bg-[#1C315F] text-[#FFFFFF] font-semibold px-6 py-2 rounded-full shadow hover:bg-[#ED3237] transition"
